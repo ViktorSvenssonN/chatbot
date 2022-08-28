8c38bc9e209d531d7897c5e880a34b375593176d
Sofia Andersson <sofia.andersson@vastansjobyskola.se>
2022-08-28 12:09:33 +0200

    added comments in our code

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+// In the js-file we have declared our variables so they are connected to the html-file.
+
 // Variables that point to selected DOM elements
+
 const chat = document.getElementById('chat');
 const inputWrapper = document.getElementById('input-wrapper')
 const input = document.getElementById('input')
 const sendBtn = document.getElementById('send')
 
 // If you need any global variables that you can use across different functions, declare them here:
-
-/* let questionNumber = 1 */
+// Here we have modifed our bot and user replies with 2 functions
 
 const botReply = (msg) => {
   showMessage(msg, 'bot')
@@ -45,11 +47,14 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight;
 }
 
-// Test Sofia //
+// Here we have a function that "moves" the chat forward, 
 
  const nextQuestion = (message) => {
   console.log('questionNumber', questionNumber) 
-
+/* Here starts our if else statment, that lets the program know where we are in the chat,
+   example "if the question is equal to one do this, else move forward. It posts the user replay
+   that has the input value that they wrote. and after 1 second the next "bot" messages shows and it 
+   moves the chat forward.*/
   if (questionNumber === 1) {
     userReply(message)
     input.value = ''
@@ -70,12 +75,15 @@ const showMessage = (message, sender) => {
 }
 
 // Starts here
+/* here is the start of the chatbot (with question one, and now you're in the if statment) */
 const greeting = () => {
   questionNumber = 1
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
   botReply("Hello there, What's your name?")
   // Just to check it out, change 'bot' to 'user' here 👆
 }
+/* this shows after one second, and the questionnumber is now 2 (questionnumber++ gives this). The reply will include a message and the user input from previous question.
+  Also three buttons will show and the next question woill depend on which button is clicked.*/  
 const showHelpTypes = (msg) => {
   questionNumber++
   botReply(
@@ -94,6 +102,8 @@ document.getElementById('jsBtn')
 .addEventListener('click', () => nextQuestion('javascript'))
 }
 
+/* Now depedning on click in previous question the type will show. And the questionNumber is now 3, and we created another if statment so the program
+catches what the user clicked, "if user clicked html do this, else do this", when user click on menu a drop down menu will show, where the user can once again click.*/ 
 const showCodeMenu = (type) => {
   questionNumber++
   botReply(
@@ -125,11 +135,12 @@ const showCodeMenu = (type) => {
       </select>
     `
   }
-
+// here we declare a const variable "select" that takes what the user selected in the drop down menu, and the "select.value" will take the user to next question.
 const select = document.getElementById('select')
   select.addEventListener('change', () => nextQuestion(select.value))
 }
-  
+/* This is question number 4. Here we declare a function that shows the value of what the user cklicked in previous question.
+Button will show depending on if you want to call, email or visit the person. Next questio will depend on what they click and show more detailed information.*/
 const showCodeHelper = (coder) => {
   questionNumber++
 
@@ -151,7 +162,9 @@ const showCodeHelper = (coder) => {
     .getElementById('house')
     .addEventListener('click', () => nextQuestion('house'))
 }
-
+/* Question number 5. This shows detailed information depending on previous question. We created an in/else if statement depending on what the user clicked.
+And we also added two buttons. Button "restart" will reload the page to let the user start from the beginning. Button "finish" will take the user to the last bot-reply where we ask if the want to 
+take our survey and a click on the button will open the suevey page. */
 const showContactinformation = (nextQuestion) => {
   questionNumber++
 
@@ -182,6 +195,9 @@ document.getElementById('finish').addEventListener('click', () => {
 
 
 // Set up your eventlisteners here
+/* This is our function for send-button that helps the user to move forward to next question instead of using the submit-form in html.
+If the user clicks it takes them to the next question and if they press enter and they have put input-value they also will be taken to
+the next question */
 sendBtn.addEventListener('click', () => nextQuestion(input.value))
 input.addEventListener('keypress', (event) => {
   if (event.key === 'Enter' && input.value) nextQuestion(input.value)
